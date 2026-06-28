27a3acf6736125489c6166845ca350cd01d2356a
KatherinaSl <katesoloduha@gmail.com>
2026-06-28 15:23:05 +0500

    feat: implement redirect to main page for auth users

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,23 +1,9 @@
-import { createClient } from "@/shared/lib/supabase/server";
 import { MainEditorScreen } from "@/widgets/main-editor-screen";
-import { redirect } from "next/navigation";
-import { Suspense } from "react";
 
 export default async function Home() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
-
-  if (error || !user) {
-    redirect("/");
-  }
-  const displayName = user.user_metadata.display_name;
   return (
     <main className="app-shell flex flex-1 px-4 py-6 md:px-6">
       <div className="app-container">
-        <Suspense>{`Welcome, ${displayName}`}</Suspense>
         <MainEditorScreen />
       </div>
     </main>
