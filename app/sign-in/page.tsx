753122d96f9c93f369d9fefd7261f5ea382fb44c
KatherinaSl <katesoloduha@gmail.com>
2026-06-28 15:23:05 +0500

    feat: implement redirect to main page for auth users

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,12 @@
+import { getUser } from "@/shared/lib/supabase/utils";
 import SignInComponent from "@/widgets/sign-in/SignIn";
+import { redirect } from "next/navigation";
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+  const user = await getUser();
+  if (user) {
+    redirect("/");
+  }
+
   return <SignInComponent />;
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,5 +1,12 @@
+import { getUser } from "@/shared/lib/supabase/utils";
 import SignUpComponent from "@/widgets/sign-up/SignUp";
+import { redirect } from "next/navigation";
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+  const user = await getUser();
+  if (user) {
+    redirect("/");
+  }
+
   return <SignUpComponent />;
 }
