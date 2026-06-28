073b17ee455b3d4bac75f2c80a95fda0a1a547b1
maiano <maiano@yandex.ru>
2026-06-28 14:22:06 +0200

    fix: move openapi workspace provider to main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,13 @@
+import { MainClientProviders } from "./main-client-providers";
+
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <MainClientProviders>
+      <main className="app-container flex-1 px-4 py-4 lg:px-8">{children}</main>
+    </MainClientProviders>
+  );
 }
