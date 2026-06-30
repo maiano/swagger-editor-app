f40822c6b508b61a47c56db4e464c7ad85c71a90
KatherinaSl <katesoloduha@gmail.com>
2026-06-30 12:07:22 +0500

    refactor: improve supabase proxy

--- shared/lib/supabase/proxy.ts
+++ shared/lib/supabase/proxy.ts
@@ -6,7 +6,7 @@ export async function updateSession(request: NextRequest) {
     request,
   });
 
-  createServerClient(
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
@@ -26,6 +26,8 @@ export async function updateSession(request: NextRequest) {
       },
     }
   );
+
+  await supabase.auth.getUser();
 
   return supabaseResponse;
 }
